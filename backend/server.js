3aabd7aa6f0d775f8f75e611d326f23d36da5927
mrsmall-owl <rstdmrshmllw@gmail.com>
2026-05-25 21:23:24 +0700

    bissmilah

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config();
 
 const express = require("express");
 const { Pool } = require("pg");
@@ -78,7 +78,7 @@ app.get("/bts", async (req, res) => {
 // JALANKAN SERVER
 // ======================
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
